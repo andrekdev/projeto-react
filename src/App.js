e67aea572944557110731b2080a6d8ceeb5485a7
andrek <zblackoez@gmail.com>
2023-04-22 01:56:53 -0400

    Adicionando componentes e passando interpolação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import Header from './Header'
 function App(){
     return(
-        <div>
+        <>
+        <Header titulo="Olá mundo"/>
             <h1>Olá Mundo!</h1>
-        </div>
+        </>
     )
 }
 
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header({titulo}){
+    return(
+        <>
+        {titulo}
+        
+        </>
+    )
+}
+
+export default Header
