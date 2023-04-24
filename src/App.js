b078843ac08300cba6da1e4ca54731d077c365f3
andrek <zblackoez@gmail.com>
2023-04-23 23:14:40 -0400

    Children

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from './Header'
+import Content from "./Content";
 function App(){
     return(
         <>
         <Header titulo="Olá mundo"/>
             <h1>Olá Mundo!</h1>
+
+            <Content titulo="Bem Vindo">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper.</p>
+            </Content>
         </>
     )
 }
